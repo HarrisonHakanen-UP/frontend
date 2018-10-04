e7f3418ac42c9fbc6ce2588e6f7721c29c450c1b
Luiz Flávio Matos <luiz.matos@montacasa.com.br>
2018-10-04 03:53:48 -0300

    [Module] Produtos Module funcionando

--- corporativos-front/src/app/produtos/produtos.component.ts
+++ corporativos-front/src/app/produtos/produtos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutosService} from '../shared/services/produtos.service';
+import { Produto } from '../shared/models/produto';
 
 @Component({
   selector: 'app-produtos',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutosComponent implements OnInit {
 
-  constructor() { }
+  produtos: Produto[];
+  produtoPost: Produto = new Produto;
+  constructor(private _produtosService: ProdutosService) { }
 
   ngOnInit() {
+    this.getProdutos()
+  }
+  getProdutos(){
+    this._produtosService.get()
+    .subscribe(
+      data => this.produtos = data,
+      err => console.log(err),
+      () => console.log(this.produtos)
+    )
+  }
+  postProduto(){
+    this._produtosService.post(this.produtoPost)
+    .subscribe(
+      data => console.log(data),
+      err => console.log(err)
+    )
   }
 
-}
+  }
